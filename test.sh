e1f202af424ab67bf3a86654c8b32e38ca56d897
Moisés Guimarães <moisesguimaraesm@gmail.com>
2013-06-02 18:51:11 -0300

    Testes

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo FATORIAL 1
+python fatorial.py 1
+
+echo FATORIAL 5
+python fatorial.py 5
+
+echo FATORIAL 10
+python fatorial.py 10
+
+echo FIBONACCI 1
+python fibonacci.py 1
+
+echo FIBONACCI 5
+python fibonacci.py 5
+
+echo FIBONACCI 10
+python fibonacci.py 10
+
+echo SOMA 1
+python soma.py 1
+
+echo SOMA 1 + 3
+python soma.py 1 3
+
+echo SOMA 1 + 3 + 5 + 7 + 9
+python soma.py 1 3 5 7 9
