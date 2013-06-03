817302f529fa4a852c5b4c1e374475fc2b5c39de
Moisés Guimarães <moisesguimaraesm@gmail.com>
2013-06-02 21:43:49 -0300

    added java suport

--- test.sh
+++ test.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
+if [ $1 = 'java' ]
+then
+    fatorial=Fatorial
+    fibonacci=Fibonacci
+    soma=Soma
+    
+    javac Fatorial.java
+    javac Fibonacci.java
+    javac Soma.java
+else
+    fatorial=fatorial.py
+    fibonacci=fibonacci.py
+    soma=soma.py
+fi
+
 echo FATORIAL 1
-python fatorial.py 1
+$1 $fatorial 1
 
 echo FATORIAL 5
-python fatorial.py 5
+$1 $fatorial 5
 
 echo FATORIAL 10
-python fatorial.py 10
+$1 $fatorial 10
 
 echo FIBONACCI 1
-python fibonacci.py 1
+$1 $fibonacci 1
 
 echo FIBONACCI 5
-python fibonacci.py 5
+$1 $fibonacci 5
 
 echo FIBONACCI 10
-python fibonacci.py 10
+$1 $fibonacci 10
 
 echo SOMA 1
-python soma.py 1
+$1 $soma 1
 
 echo SOMA 1 + 3
-python soma.py 1 3
+$1 $soma 1 3
 
 echo SOMA 1 + 3 + 5 + 7 + 9
-python soma.py 1 3 5 7 9
+$1 $soma 1 3 5 7 9
